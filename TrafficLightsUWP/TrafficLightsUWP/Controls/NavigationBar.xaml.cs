972d1017a5baa5d13ef994be75e7c6cf25d3d47a
CasDecelle <cas.decelle@gmail.com>
2016-11-02 10:34:11 +0100

    set up navigation

---
+++ TrafficLightsUWP/TrafficLightsUWP/Controls/NavigationBar.xaml.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TrafficLightsUWP.Controls
+{
+    public sealed partial class NavigationBar : UserControl
+    {
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return (RelayCommand)GetValue(AddCommandProperty);
+            }
+
+            set
+            {
+                SetValue(AddCommandProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty AddCommandProperty = DependencyProperty.Register(
+          "AddCommand",
+          typeof(RelayCommand),
+          typeof(NavigationBar),
+          new PropertyMetadata(null)
+        );
+
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return (RelayCommand)GetValue(EditCommandProperty);
+            }
+
+            set
+            {
+                SetValue(EditCommandProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty EditCommandProperty = DependencyProperty.Register(
+          "EditCommand",
+          typeof(RelayCommand),
+          typeof(NavigationBar),
+          new PropertyMetadata(null)
+        );
+
+        public RelayCommand HomeCommand
+        {
+            get
+            {
+                return (RelayCommand)GetValue(HomeCommandProperty);
+            }
+
+            set
+            {
+                SetValue(HomeCommandProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty HomeCommandProperty = DependencyProperty.Register(
+          "HomeCommand",
+          typeof(RelayCommand),
+          typeof(NavigationBar),
+          new PropertyMetadata(null)
+        );
+
+        public NavigationBar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
--- TrafficLightsUWP/TrafficLightsUWP/ViewModels/BaseViewModel.cs
+++ TrafficLightsUWP/TrafficLightsUWP/ViewModels/BaseViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace TrafficLightsUWP.ViewModels
 {
     public class BaseViewModel : ViewModelBase
     {
+        public RelayCommand HomeCommand { get { return new RelayCommand(NavigateToHome); } }
+        public RelayCommand AddCommand { get { return new RelayCommand(NavigateToAddTraficLight); } }
+        public RelayCommand EditCommand { get { return new RelayCommand(NavigateToEditTraficLight); } }
+
+        public void NavigateToHome()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(DashboardView));
+        }
+
+        public void NavigateToAddTraficLight()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(DashboardView));
+        }
+
+        public void NavigateToEditTraficLight()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(DashboardView));
+        }
     }
 }
