4f0fb81520d8417c618ad85535a04860594a38e9
dverhoeven1200 <david.verhoeven@realdolmen.com>
2016-11-02 10:13:58 +0100

    Added services for Michiel

---
+++ TrafficLightsUWP/TrafficLightsUWP/Services/LocationService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficLightsUWP.Models;
+using Windows.Devices.Geolocation;
+
+namespace TrafficLightsUWP.Services
+{
+    
+    
+
+    public class LocationService
+    {
+        private List<Location> fakeLocations = new List<Location>()
+        {
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4070"), Name = "Zwarte kat Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4071"), Name = "Zwembad Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4072"), Name = "McDo Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4073"), Name = "Stroppen Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4074"), Name = "Station Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4075"), Name = "Don Bosco Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4076"), Name = "Heilig Hart Halle", Coordinates = null },
+            new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4077"), Name = "Atheneneum Halle", Coordinates = null },
+
+        };
+
+        public IEnumerable<Location> GetAllLocations()
+        {
+            return fakeLocations;
+        }
+
+        public Location GetLocationById(Guid id)
+        {
+            return fakeLocations
+                .Where(l => l.Id == id)
+                .FirstOrDefault();
+        }
+
+        public Location AddLocation(Location location)
+        {
+            location.Id = Guid.NewGuid();
+            fakeLocations.Add(location);
+            return location;
+        }
+
+        public void SaveLocation(Location location)
+        {
+            Location oldLocation = fakeLocations.
+                Where(l => l.Id == location.Id)
+                .FirstOrDefault();
+
+            if (oldLocation == null)
+            {
+                AddLocation(location);
+                return;
+            }
+
+            oldLocation.Name = location.Name;
+            oldLocation.Coordinates = location.Coordinates;                 
+        }
+
+        public void DeleteLocation(Location location)
+        {
+            fakeLocations.Remove(location);
+        }
+    }
+}
---
+++ TrafficLightsUWP/TrafficLightsUWP/Services/TrafficLightService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficLightsUWP.Models;
+
+namespace TrafficLightsUWP.Services
+{
+    public class TrafficLightService
+    {
+
+        private List<TrafficLight> fakeTrafficLights = new List<TrafficLight>()
+        {
+           new TrafficLight
+           {
+               Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4080"),
+               ActiveSince = DateTime.Now,
+               Direction = TrafficLightDirection.E,
+               IsPlaced = true,
+               PlacementDate = DateTime.Now,
+               Status = TrafficLightStatus.Active,
+               NumberOfSecsGreen = 30,
+               NumberOfSecsOrange= 7,
+               NumberOfSecsRed = 1000,
+               Location = new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4070"), Name = "Zwarte kat Halle", Coordinates = null }
+
+           },
+           new TrafficLight
+           {
+               Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4081"),
+               ActiveSince = DateTime.Now,
+               Direction = TrafficLightDirection.N,
+               IsPlaced = false,
+               Status = TrafficLightStatus.Inactive,
+               NumberOfSecsGreen = 30,
+               NumberOfSecsOrange= 7,
+               NumberOfSecsRed = 1000,
+               Location = new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4071"), Name = "Zwembad Halle", Coordinates = null }
+
+           },
+           new TrafficLight
+           {
+               Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4082"),
+               ActiveSince = DateTime.Now,
+               Direction = TrafficLightDirection.S,
+               IsPlaced = true,
+               PlacementDate = DateTime.Now,
+               Status = TrafficLightStatus.Defective,
+               NumberOfSecsGreen = 30,
+               NumberOfSecsOrange= 7,
+               NumberOfSecsRed = 1000,
+               Location =  new Location { Id = new Guid("7b1911aa-933f-48ce-b431-5bbff70c4072"), Name = "McDo Halle", Coordinates = null }
+
+           }
+        };
+
+
+        public IEnumerable<TrafficLight> GetAllTrafficLights()
+        {
+            return fakeTrafficLights;
+        }
+
+        public TrafficLight GetTrafficLightById(Guid id)
+        {
+            return fakeTrafficLights
+                .Where(l => l.Id == id)
+                .FirstOrDefault();
+        }
+
+        public TrafficLight AddTrafficLight(TrafficLight TrafficLight)
+        {
+            TrafficLight.Id = Guid.NewGuid();
+            fakeTrafficLights.Add(TrafficLight);
+            return TrafficLight;
+        }
+
+        public void SaveTrafficLight(TrafficLight TrafficLight)
+        {
+            TrafficLight oldTrafficLight = fakeTrafficLights.
+                Where(l => l.Id == TrafficLight.Id)
+                .FirstOrDefault();
+
+            if (oldTrafficLight == null)
+            {
+                AddTrafficLight(TrafficLight);
+                return;
+            }
+
+            oldTrafficLight.Name = TrafficLight.Name;
+            oldTrafficLight.Coordinates = TrafficLight.Coordinates;
+        }
+
+        public void DeleteTrafficLight(TrafficLight TrafficLight)
+        {
+            fakeTrafficLights.Remove(TrafficLight);
+        }
+    }
+}
